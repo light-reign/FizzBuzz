64d092ec095c0ffb75024519be13abb785cb65fa
Stevo <stevo@scoldfusion.com>
2015-05-09 12:35:33 -0500

    Making fizz buzz more modular

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,20 +1,24 @@
-def fizz_buzz(num_list):
+def output_list(my_list):
+    for item in my_list:
+        print item,
+
+def fizz_buzz(fizz, buzz, num_list):
     result = []
     for num in num_list:
         output = ''
-        fizz = (num % 3 == 0)
-        buzz = (num % 5 == 0)
-        if fizz:
+        should_fizz = (num % fizz == 0)
+        should_buzz = (num % buzz == 0)
+        if should_fizz:
             output += "Fizz"
 
-        if buzz:
+        if should_buzz:
             output += "Buzz"
 
-        if not fizz and not buzz:
+        if not should_fizz and not should_buzz:
             output = num
 
         result.append(output)
         
     return result
 
-print fizz_buzz(range(0,101))
+output_list(fizz_buzz(3, 5, range(0,101)))
